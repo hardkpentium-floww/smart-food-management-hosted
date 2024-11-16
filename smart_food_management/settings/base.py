b36ae7f0cfebf783844e235f9b1c0d920e994bad
dw-dev-012 <dw-dev012@localhost>
2024-11-16 17:56:07 +0530

    add

--- smart_food_management/settings/base.py
+++ smart_food_management/settings/base.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS = [
     "smart-food-management-staging.apigateway.in",
     "127.0.0.1",
     "localhost",
-    "heavy-suits-beam.loca.lt"
+    "fluffy-baths-cough.loca.lt"
 ]
 
 ROOT_URLCONF = 'smart_food_management.urls'
